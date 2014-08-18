461adb5dbb962fe625c31efd468b29b12f97b62b
Bruce Merry <bmerry@ska.ac.za>
2014-08-18 11:32:28 +0200

    Add basic tests of the backgrounding

---
+++ katsdpsigproc/rfi/test_background.py
@@ -0,0 +1,33 @@
+import numpy as np
+from ..accel import Array, DeviceArray
+import background
+import pycuda.autoinit
+from nose.tools import *
+
+def setup():
+    global _vis, _vis_big
+    shape = (17, 13)
+    _vis = np.array([[1.25, 1.5j, 1.0, 2.0, -1.75, 2.0]]).T.astype(np.complex64)
+    _vis_big = (np.random.randn(*shape) + np.random.randn(*shape) * 1j).astype(np.complex64)
+
+class TestBackgroundMedianFilterHost(object):
+    def setup(self):
+        self.background = background.BackgroundMedianFilterHost(3)
+
+    def test(self):
+        out = self.background(_vis)
+        ref = np.array([[0.0, 0.25, -0.5, 0.25, -0.25, 0.25]]).T.astype(np.float32)
+        np.testing.assert_equal(ref, out)
+
+def test_device_classes():
+    yield check_device_class, background.BackgroundMedianFilterDevice, 5, (128, 4)
+
+def check_device_class(cls, width, device_args=(), device_kw={}):
+    bg_host = cls.host_class(width)
+    bg_device = background.BackgroundHostFromDevice(
+            cls(pycuda.autoinit.context, width, *device_args, **device_kw))
+    out_host = bg_host(_vis_big)
+    out_device = bg_device(_vis_big)
+    print np.fmax(out_host - out_device, 1e-5)
+    # Uses an abs tolerance because backgrounding subtracts nearby values
+    np.testing.assert_allclose(out_host, out_device, atol=1e-6)
