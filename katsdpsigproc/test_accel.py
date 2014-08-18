529ce3b9ac84c77fbbf9577540ca95d6fee2906f
Bruce Merry <bmerry@ska.ac.za>
2014-08-18 11:32:09 +0200

    Modify tests use to use nose instead of unittest

--- katsdpsigproc/test_accel.py
+++ katsdpsigproc/test_accel.py
@@ -1,11 +1,11 @@
 import numpy as np
-import unittest
 from .accel import Array, DeviceArray
 import pycuda.autoinit
 import pycuda.driver as cuda
+from nose.tools import *
 
-class TestArray(unittest.TestCase):
-    def setUp(self):
+class TestArray(object):
+    def setup(self):
         self.shape = (17, 13)
         self.padded_shape = (32, 16)
         self.constructed = Array(shape=self.shape, dtype=np.int32, padded_shape=self.padded_shape)
@@ -13,13 +13,13 @@ class TestArray(unittest.TestCase):
         self.sliced = self.constructed[2:4, 3:5]
 
     def test_safe(self):
-        self.assertTrue(Array.safe(self.constructed))
-        self.assertFalse(Array.safe(self.view))
-        self.assertFalse(Array.safe(self.sliced))
-        self.assertFalse(Array.safe(np.zeros(self.shape)))
+        assert Array.safe(self.constructed)
+        assert not Array.safe(self.view)
+        assert not Array.safe(self.sliced)
+        assert not Array.safe(np.zeros(self.shape))
 
-class TestDeviceArray(unittest.TestCase):
-    def setUp(self):
+class TestDeviceArray(object):
+    def setup(self):
         self.ctx = pycuda.autoinit.context
         self.shape = (17, 13)
         self.padded_shape = (32, 16)
@@ -31,13 +31,13 @@ class TestDeviceArray(unittest.TestCase):
                 padded_shape=self.padded_shape)
 
     def test_strides(self):
-        self.assertEqual(self.strides, self.array.strides)
+        assert_equal(self.strides, self.array.strides)
 
     def test_empty_like(self):
         ary = self.array.empty_like()
-        self.assertEqual(self.shape, ary.shape)
-        self.assertEqual(self.strides, ary.strides)
-        self.assertTrue(Array.safe(ary))
+        assert_equal(self.shape, ary.shape)
+        assert_equal(self.strides, ary.strides)
+        assert Array.safe(ary)
 
     def test_set_get(self):
         ary = np.random.randint(0, 100, self.shape).astype(np.int32)
@@ -46,7 +46,7 @@ class TestDeviceArray(unittest.TestCase):
         buf = np.zeros(self.padded_shape, dtype=np.int32)
         cuda.memcpy_dtoh(buf, self.array.buffer.gpudata)
         buf = buf[0:self.shape[0], 0:self.shape[1]]
-        assert np.all(ary == buf)
+        np.testing.assert_equal(ary, buf)
         # Check that it matches get
         buf = self.array.get()
-        assert np.all(ary == buf)
+        np.testing.assert_equal(ary, buf)
