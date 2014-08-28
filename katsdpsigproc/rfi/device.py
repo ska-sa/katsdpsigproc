5c2781839f0962967c3e6f1acd58b3f6cfd09310
Bruce Merry <bmerry@ska.ac.za>
2014-08-28 16:38:10 +0200

    Remove some dead code

--- katsdpsigproc/rfi/device.py
+++ katsdpsigproc/rfi/device.py
@@ -244,7 +244,6 @@ class ThresholdMADTDevice(object):
         self.factor = 1.4826 * n_sigma
         self.flag_value = flag_value
         self.max_channels = max_channels
-        self._vt = 16
         self._wgsx = 512
         self._vt = (max_channels + self._wgsx - 1) // self._wgsx
         program = accel.build(command_queue.context, 'rfi/threshold_mad_t.mako',
