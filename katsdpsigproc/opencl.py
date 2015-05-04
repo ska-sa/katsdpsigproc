7ea8a087fda8cb2bda355d4129af1f92b7627e6d
Bruce Merry <bmerry@ska.ac.za>
2015-05-04 11:07:35 +0200

    Update OpenCL SVM stubs to match CUDA interface

--- katsdpsigproc/opencl.py
+++ katsdpsigproc/opencl.py
@@ -283,7 +283,10 @@ class Context(object):
         else:
             return np.empty(shape, dtype)
 
-    def allocate_svm(self, shape, dtype):
+    def allocate_svm_raw(self, n_bytes):
+        raise NotImplementedError("PyOpenCL does not support OpenCL Shared Virtual Memory")
+
+    def allocate_svm(self, shape, dtype, raw=None):
         raise NotImplementedError("PyOpenCL does not support OpenCL Shared Virtual Memory")
 
     def create_command_queue(self, profile=False):
