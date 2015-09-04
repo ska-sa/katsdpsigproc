be4e928ad175a69b403ececf4222608168014bd1
Bruce Merry <bmerry@ska.ac.za>
2015-09-04 10:13:14 +0200

    Fix CUDA doing transfers on default stream
    There were two issues here:
    - The CUDA wrapper passed synchronous transfers straight through to
      PyCUDA, which previously did not allow a stream to be specified.
      This is now avoided by always doing an async transfer and then
      manually waiting. This might have higher CPU overhead, but it avoids
      putting in a device-wide barrier.
    - PyCUDA 2015.1.3 has a bug in set_async, in which it ignores the
      stream. Worked around by calling the set function with async=True.

--- katsdpsigproc/cuda.py
+++ katsdpsigproc/cuda.py
@@ -167,22 +167,19 @@ class CommandQueue(object):
 
     def enqueue_read_buffer(self, buffer, data, blocking=True):
         with self.context:
+            # CUDA doesn't support synchronous transfers sequenced in a stream
+            # (PyCUDA simply doesn't pass on the stream argument), so use an
+            # async transfer and then block if necessary.
+            buffer.get(data, async=True, stream=self._pycuda_stream)
             if blocking:
-                # TODO: PyCUDA doesn't take a stream argument here!
-                buffer.get(data)
-            else:
-                # The order of arguments in PyCUDA 2014.1 doesn't match the
-                # documentation (https://github.com/inducer/pycuda/issues/58).
-                # Rather than guessing which will get fixed, pass by keyword.
-                buffer.get_async(ary=data, stream=self._pycuda_stream)
+                self._pycuda_stream.synchronize()
 
     def enqueue_write_buffer(self, buffer, data, blocking=True):
         with self.context:
+            # See comment in enqueue_read_buffer
+            buffer.set(data, async=True, stream=self._pycuda_stream)
             if blocking:
-                # TODO: PyCUDA doesn't take a stream argument here!
-                buffer.set(data)
-            else:
-                buffer.set_async(data, stream=self._pycuda_stream)
+                self._pycuda_stream.synchronize()
 
     def enqueue_kernel(self, kernel, args, global_size, local_size):
         assert len(global_size) == len(local_size)
