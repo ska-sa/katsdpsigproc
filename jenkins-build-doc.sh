af0f67facce34f1d77f5f3bd60dd34dc73e9d60d
Bruce Merry <bmerry@ska.ac.za>
2015-11-06 13:19:11 +0200

    Force specific pyopencl version in Jenkins doc build

--- jenkins-build-doc.sh
+++ jenkins-build-doc.sh
@@ -3,6 +3,6 @@ set -e -x
 pip install -U pip setuptools wheel
 pip install numpy  # Some requirements need it already installed
 pip install -r requirements.txt
-pip install pycuda pyopencl
+pip install pycuda 'pyopencl==2015.1'    # 2015.2.1 fails to compile
 rm -rf doc/_build
 make -C doc html
