398e9850c7a2685b3c7ecf5459f8da143666de10
Bruce Merry <bmerry@ska.ac.za>
2020-02-27 17:03:50 +0200

    Add random_offsets option to sample_image

--- katsdpsigproc/zscale.py
+++ katsdpsigproc/zscale.py
@@ -1,18 +1,40 @@
 """A Python implementation of the IRAF/sa9 zscale algorithm."""
 
+import random
 from typing import Tuple
 
 import numpy as np
 
 
-def sample_image(image: np.ndarray, max_samples=100000) -> np.ndarray:
+def sample_image(image: np.ndarray, max_samples: int = 100000,
+                 random_offsets: bool = False) -> np.ndarray:
     """Take regularly-spaced samples from a 2D image.
 
-    This provides suitable input for :func:`zscale`.
+    This provides suitable input for :func:`zscale`. The samples are taken from
+    a regular grid.
+
+    Parameters
+    ----------
+    image
+        2D array of float
+    max_samples
+        Maximum number of samples to return. The actual number returned may be
+        less if the image is smaller or if there are non-finite samples (which
+        are discarded)
+    random_offsets
+        If true, the placement of the grid is chosen randomly; otherwise, it
+        start from the pixel at (0, 0). Enabling this is useful when combining
+        samples from multiple images.
     """
     nx, ny = image.shape
-    stride = int(max(1., np.sqrt(nx * ny / float(max_samples))))
-    samples = image[::stride, ::stride].flatten()
+    stride = int(max(1.0, np.sqrt(nx * ny / max_samples)))
+    if random_offsets:
+        offset_x = random.randrange(0, stride)
+        offset_y = random.randrange(0, stride)
+    else:
+        offset_x = 0
+        offset_y = 0
+    samples = image[offset_x::stride, offset_y::stride].flatten()
     # Remove blanked pixels
     samples = samples[np.isfinite(samples)]
     # Force the maximum number of samples
