eb458c587e006286342db89e61b8127a573f4aad
Bruce Merry <bmerry@ska.ac.za>
2020-05-12 14:46:20 +0200

    Write graphviz output as SVG

--- doc/conf.py
+++ doc/conf.py
@@ -116,6 +116,8 @@ pygments_style = 'sphinx'
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
 
+graphviz_output_format = 'svg'
+
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
