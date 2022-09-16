f79037a2a63ca25d685cae657fa5faa099ccc099
richarms <richarms@ska.ac.za>
2022-09-16 09:37:14 +0000

    wip: autotune nearnest matching tests

--- katsdpsigproc/test/test_tune.py
+++ katsdpsigproc/test/test_tune.py
@@ -1,16 +1,39 @@
 """Tests for :mod:`katsdpsigproc.tune`."""
 
+import os
 import sys
 import traceback
 import threading
 from unittest import mock
-from typing import NoReturn, Any
+from unittest.mock import MagicMock
+from typing import NoReturn, Any, Mapping
 
 from nose.tools import assert_equal, assert_raises
 import sqlite3
 
 from .. import tune
 from ..abc import AbstractContext
+
+
+def test_autotune_understanding_lambda() -> None:
+    cartesian = []
+    cartesian_lock = threading.Lock()
+
+    def generate(x_dim, y_dim):
+        with cartesian_lock:
+            cartesian.append((x_dim, y_dim))
+        return lambda scoring_function: (1+16*1024)-(x_dim * y_dim) if (x_dim * y_dim) < (1+16*1024) else (1+16*1024)
+
+    opt = tune.autotune(generate, time_limit=0.1, x_dim = [2,8,64,128], y_dim = [4,16,32,256])
+    assert_equal({'x_dim': 64, 'y_dim': 256}, opt)
+
+
+@mock.patch.dict(os.environ, {"KATSDPSIGPROC_TUNE_MATCH": "nearest"})
+def test_device_fallback() -> None:
+    # autotune a set of values to a function, storing results as usual in the sql database
+    # amend the results to point to a similar but different device
+    # check that the autotune uses the cached version
+    tune.autotune(lambda x, y: lambda prod: x*y, x=[1],y=[3])
 
 
 def test_autotune_basic() -> None:
@@ -34,7 +57,7 @@ def test_autotune_empty() -> None:
 
 
 class CustomError(RuntimeError):
-    pass
+    ...
 
 
 def test_autotune_some_raise() -> None:
@@ -113,3 +136,24 @@ class TestAutotuner:
         self.autotune_mock.assert_called_once_with(context, 'xyz')
         assert_equal(tuning, ret1)
         assert_equal(tuning, ret2)
+
+
+@mock.patch.dict(os.environ, {"KATSDPSIGPROC_TUNE_MATCH": "nearest"})
+class TestDeviceFallback:
+    """Tests for device fallback"""
+    generate = mock.Mock()
+
+    @classmethod
+    @tune.autotuner(test={'a': 3, 'b': -1})
+    def autotune(cls, context: AbstractContext, param: str) -> Mapping[str, Any]:
+        return tune.autotune(generate, wgs=[32])
+    
+    @mock.patch('katsdpsigproc.tune._query')
+    @mock.patch('katsdpsigproc.tune._fetch')
+    def test(self, query_mock: mock.Mock, fetch_mock: mock.Mock) -> None: 
+        context = mock.NonCallableMock()
+        context.device.driver_version = 'mock version'
+        context.device.platform_name = 'mock platform'
+        context.device.name = 'mock device'
+        self.autotune(context, 'WGS = [32]')
+        
